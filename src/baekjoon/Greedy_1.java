4384ca7ef6fc164ede6e5c1da5e9e91b621b36b6
guhyeon-kwon <ghkwon0130@gmail.com>
2021-09-03 22:27:08 +0900

    greedy_1 update
    주석추가

--- src/baekjoon/Greedy_1.java
+++ src/baekjoon/Greedy_1.java
@@ -22,23 +22,31 @@ public class Greedy_1 {
 
     public static void greedy() throws IOException {
 
+        // 값 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        // 첫번째 줄 읽어와서 " " 기준으로 쪼개서 가져옴
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
+        // 동전의 종류 갯수
         int N = Integer.parseInt(st.nextToken());
+        // 가치의 합
         int K = Integer.parseInt(st.nextToken());
 
+        // 동전 종류 갯수만큼 배열 공간 확보
         int[] coin = new int[N];
 
+        // 동전 종류 갯수만큼 for문 돌면서 코인 리스트에 동전의 가치 입력
         for(int i = 0; i  < N; i++){
             coin[i] = Integer.parseInt(br.readLine());
         }
 
+        // 결과로 출력할 동전 개수
         int count = 0;
 
+        // 동전의 가치가 오름차순으로 주어지므로 뒤에서 부터 가져옴
         for(int i = N - 1; i >= 0; i--) {
-
+            // 동전의 가치가 가치의 합보다 작아야함
             if(coin[i] <= K) {
                 count += (K / coin[i]);
                 K = K % coin[i];
